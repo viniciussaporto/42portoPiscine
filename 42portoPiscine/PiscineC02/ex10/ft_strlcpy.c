e38e122e83a655a60e4fb7d8f8d5d6804bcf501a
Vinícius Sá Porto <73244946+viniciussaporto@users.noreply.github.com>
2022-07-21 22:41:32 +0100

    Add files via upload

---
+++ 42portoPiscine/PiscineC02/ex10/ft_strlcpy.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return(i);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	a;
+
+	i = 0;
+	a = ft_strlen(src);
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (a);
+}
+
+int	main(void)
+{
+	char	src[] = "Fluminense";
+	char	dest[] = "campeão";
+	unsigned int	size = 11;
+
+	printf("%d e %s", ft_strlcpy(dest, src, size), dest);
+}
